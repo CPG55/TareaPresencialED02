14dcc6381450ef165c9b5ef64a07f7f1e82c26f9
carlos <carlos@carlos-K53SV>
2016-11-15 18:54:14 +0100

    Añadida clase HolaMundo

---
+++ HolaMundo/src/HolaMundo.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+
+
+public class HolaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date ahora = new Date ();
+		System.out.printf("%s %tT",  "Son las: " , ahora);
+		
+		System.out.println("Estamos en la practica presencial de Entornos");
+		
+	}
+
+}
